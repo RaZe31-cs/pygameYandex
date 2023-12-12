7f5eedc13f99bd07c4d1a9849c67e1ab8ba9e11d
RaZe31-cs <gri.dubrovskih@gmail.com>
2023-12-12 15:58:52 +0500

    Добавлено окно регистрации, но пока что ввод плохо работает

--- start_window.py
+++ start_window.py
@@ -2,6 +2,61 @@ import os.path
 import pygame
 
 
+class InputLine:
+    def __init__(self, x: int, y: int, width: int, height: int, text_bg: str):
+        self.rect = pygame.rect.Rect(x, y, width, height)
+
+        self.font = pygame.font.Font(None, height // 6 * 3)
+        self.text = text_bg
+        self.user_input = ''
+
+        self.enter = False
+
+    def draw(self, screen: pygame.surface.Surface):
+        pygame.draw.rect(screen, '#03045e', self.rect, width=3)
+        if not self.enter:
+            self.inp_text = self.font.render(self.text, False, '#3c81f0')
+        else:
+            self.inp_text = self.font.render(self.user_input, False, '#03045e')
+        self.text_rect = self.inp_text.get_rect(center=self.rect.center)
+
+        """
+        Ниже стирается старый текст путем наложение такого же текста, цветом заднего фона
+        ДОДЕЛАТЬ
+        !
+        !
+        !
+        !
+        !
+        """
+        text1 = self.font.render(self.text, False, '#ade8f4')
+        text2 = self.font.render(self.user_input, False, '#ade8f4')
+        text1_rect = text1.get_rect(center=self.rect.center)
+        text2_rect = text2.get_rect(center=self.rect.center)
+        screen.blit(text1, text1_rect)
+        screen.blit(text2, text2_rect)
+        """
+        !
+        !
+        !
+        !
+        ДОДЕЛАТЬ
+        """
+
+        screen.blit(self.inp_text, self.text_rect)
+
+    def is_clicked(self):
+        mouse = pygame.mouse.get_pos()
+        if self.rect.x <= mouse[0] <= self.rect.x + self.rect.w and self.rect.y <= mouse[
+            1] <= self.rect.y + self.rect.h:
+            return True
+        return False
+
+    def input(self, event):
+        if self.enter:
+            self.user_input += chr(event.key)
+
+
 class Button:
     def __init__(self, x, y, width, height, text, normal_bgcolor, hovered_bgcolor,
                  normal_textcolor, hovered_textcolor):
@@ -28,8 +83,9 @@ class Button:
         mouse = pygame.mouse.get_pos()
         if self.rect.x <= mouse[0] <= self.rect.x + self.rect.w and self.rect.y <= mouse[
             1] <= self.rect.y + self.rect.h:
-                return True
+            return True
         return False
+
 
 class BaseWindow:
     def __init__(self):
@@ -50,7 +106,7 @@ class MainWindow(BaseWindow):
         self.screen.fill('#ade8f4')
         pygame.display.set_caption('Главный экран')
 
-        btn_width, btn_height = 200, 50
+        btn_width, btn_height = 210, 50
         btn_login_x, btn_login_y = self.size[0] / 3 - btn_width / 2, self.size[1] / 3 + btn_height
         btn_reg_x, btn_reg_y = self.size[0] - self.size[0] / 3 - btn_width / 2, self.size[1] / 3 + btn_height
         btn_records_x, btn_records_y = self.size[0] / 2 - btn_width / 2, self.size[1] / 2 + btn_height / 2
@@ -96,8 +152,24 @@ class RegistrationWindow(BaseWindow):
         super().__init__()
         pygame.display.set_caption('Регистрация')
 
+        self.screen.fill('#ade8f4')
+
+        inp_width, inp_height = 210, 40
+
+        inp_user_name_x, inp_user_name_y = self.size[0] / 6, self.size[1] / 10 * 2
+        inp_password_x, inp_password_y = self.size[0] / 6, self.size[1] / 10 * 4
+        inp_proof_password_x, inp_proof_password_y = self.size[0] / 6, self.size[1] / 10 * 6
+
+        self.user_name = InputLine(inp_user_name_x, inp_user_name_y, inp_width, inp_height, 'Имя пользователя')
+        self.password = InputLine(inp_password_x, inp_password_y, inp_width, inp_height, 'Пароль')
+        self.proof_password = InputLine(inp_proof_password_x, inp_proof_password_y, inp_width, inp_height,
+                                        'Подтверждение пароля')
+
+        self.inp_group = (self.user_name, self.password, self.proof_password)
+
     def draw(self):
-        pass
+        for i in self.inp_group:
+            i.draw(self.screen)
 
 
 class RecordsWindow(BaseWindow):
@@ -126,6 +198,22 @@ if __name__ == '__main__':
                         current_window = RegistrationWindow()
                     elif current_window.btn_records.is_clicked():
                         current_window = RecordsWindow()
+                elif isinstance(current_window, RegistrationWindow):
+                    for input_line in current_window.inp_group:
+                        if input_line.is_clicked():
+                            input_line.enter = True
+                            print('нажата')
+                            break
+                    else:
+                        for input_line in current_window.inp_group:
+                            input_line.enter = False
+                            print('все сброшены')
+            elif event.type == pygame.KEYDOWN:
+                if isinstance(current_window, RegistrationWindow):
+                    for input_line in current_window.inp_group:
+                        if input_line.enter == True:
+                            input_line.input(event)
+
         current_window.draw()
         pygame.display.flip()
     pygame.quit()
