6a6ac5c91b658faef1f5e031d3152cb5aa5d9428
chrpnnkv <varyacherep2006@gmail.com>
2023-12-12 12:23:28 +0500

    Стартовое окно

---
+++ start_window.py
@@ -0,0 +1,131 @@
+import os.path
+import pygame
+
+
+class Button:
+    def __init__(self, x, y, width, height, text, normal_bgcolor, hovered_bgcolor,
+                 normal_textcolor, hovered_textcolor):
+        self.rect = pygame.rect.Rect(x, y, width, height)
+
+        self.font = pygame.font.Font(None, height // 6 * 4)
+        self.text = text
+
+        self.colors = {'normal_bg': normal_bgcolor, 'hovered_bg': hovered_bgcolor, 'normal_text': normal_textcolor,
+                       'hovered_text': hovered_textcolor}
+
+    def draw(self, screen):
+        mouse = pygame.mouse.get_pos()
+        bg_color, text_color = self.colors['normal_bg'], self.colors['normal_text']
+        if self.is_clicked():
+            bg_color, text_color = self.colors['hovered_bg'], self.colors['hovered_text']
+        screen.fill(bg_color, self.rect)
+
+        self.btn_text = self.font.render(self.text, True, text_color)
+        self.text_rect = self.btn_text.get_rect(center=self.rect.center)
+        screen.blit(self.btn_text, self.text_rect)
+
+    def is_clicked(self):
+        mouse = pygame.mouse.get_pos()
+        if self.rect.x <= mouse[0] <= self.rect.x + self.rect.w and self.rect.y <= mouse[
+            1] <= self.rect.y + self.rect.h:
+                return True
+        return False
+
+class BaseWindow:
+    def __init__(self):
+        pygame.init()
+
+        self.size = (800, 600)
+
+        self.screen = pygame.display.set_mode(self.size)
+        self.group_sprite = None
+
+    def add_group_sprite(self, group):
+        self.group_sprite = group
+
+
+class MainWindow(BaseWindow):
+    def __init__(self):
+        super().__init__()
+        self.screen.fill('#ade8f4')
+        pygame.display.set_caption('Главный экран')
+
+        btn_width, btn_height = 200, 50
+        btn_login_x, btn_login_y = self.size[0] / 3 - btn_width / 2, self.size[1] / 3 + btn_height
+        btn_reg_x, btn_reg_y = self.size[0] - self.size[0] / 3 - btn_width / 2, self.size[1] / 3 + btn_height
+        btn_records_x, btn_records_y = self.size[0] / 2 - btn_width / 2, self.size[1] / 2 + btn_height / 2
+
+        normal_bg, hovered_bg = '#48cae4', '#00b4d8'
+        normal_text, hovered_text = '#03045e', '#ffffff'
+
+        self.btn_login = Button(btn_login_x, btn_login_y, btn_width, btn_height, 'Войти в аккаунт', normal_bg,
+                                hovered_bg, normal_text, hovered_text)
+
+        self.btn_reg = Button(btn_reg_x, btn_reg_y, btn_width, btn_height, 'Создать аккаунт', normal_bg,
+                              hovered_bg, normal_text, hovered_text)
+
+        self.btn_records = Button(btn_records_x, btn_records_y, btn_width, btn_height, 'Таблица рекордов', normal_bg,
+                                  hovered_bg, normal_text, hovered_text)
+
+        self.btn_group = [self.btn_login, self.btn_reg, self.btn_records]
+
+    def display_text(self):
+        f1 = pygame.font.Font(os.path.join('data', 'Aguante-Regular.otf'), 60)
+        text = f1.render('UNDERWATER', True, '#0077b6')
+        x, y = self.size[0] / 2 - text.get_width() / 2, self.size[1] / 2 - text.get_width() / 2
+        text_w, text_h = text.get_width(), text.get_height()
+        self.screen.blit(text, (x, y))
+
+    def draw(self):
+        self.display_text()
+        for btn in self.btn_group:
+            btn.draw(self.screen)
+
+
+class LoginWindow(BaseWindow):
+    def __init__(self):
+        super().__init__()
+        pygame.display.set_caption('Логин')
+
+    def draw(self):
+        pass
+
+
+class RegistrationWindow(BaseWindow):
+    def __init__(self):
+        super().__init__()
+        pygame.display.set_caption('Регистрация')
+
+    def draw(self):
+        pass
+
+
+class RecordsWindow(BaseWindow):
+    def __init__(self):
+        super().__init__()
+        pygame.display.set_caption('Рекорды')
+
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    current_window = MainWindow()
+    running = True
+    clock = pygame.time.Clock()
+    FPS = 60
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if isinstance(current_window, MainWindow):
+                    if current_window.btn_login.is_clicked():
+                        current_window = LoginWindow()
+                    elif current_window.btn_reg.is_clicked():
+                        current_window = RegistrationWindow()
+                    elif current_window.btn_records.is_clicked():
+                        current_window = RecordsWindow()
+        current_window.draw()
+        pygame.display.flip()
+    pygame.quit()
