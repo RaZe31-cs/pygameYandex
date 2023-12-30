2fc2a21b74e4bae4915941a68619c368b6c5fcc5
RaZe31-cs <gri.dubrovskih@gmail.com>
2023-12-30 22:44:28 +0500

    Изменение фона, добавление звуков, окно с концовкой

--- level.py
+++ level.py
@@ -22,6 +22,8 @@ pygame.init()
 icon = pygame.image.load(os.path.join('data', 'icon.png'))
 pygame.display.set_icon(icon)
 level = load_level('levels/level1.csv')
+len_level_x = len(level[0])
+len_level_y = len(level)
 tile_size = 40
 size = width, height = 1280, len(level) * tile_size
 screen = pygame.display.set_mode(size)
@@ -50,10 +52,41 @@ def import_folder(path):
     images = []
     for _, __, img_file in os.walk(path):
         for img in img_file:
-            fullname = path + '/' + img
+            fullname = os.path.join(path, img)
             image = pygame.image.load(fullname).convert_alpha()
             images.append(image)
     return images
+
+
+class Sounds:
+    def __init__(self, path_running=None, path_jump=None, path_drop=None, path_star=None, path_take_a_life=None):
+        self.set_sound_running(path_running)
+        self.set_sound_star(path_star)
+        self.set_sound_jump(path_jump)
+        self.set_sound_drop(path_drop)
+        self.set_sound_take_a_life(path_take_a_life)
+
+    def set_sound_running(self, path):
+        self.music_running = pygame.mixer.music
+        self.music_running.load(os.path.join('data', 'sound', path))
+        self.music_running.set_volume(0.05)
+
+    def set_sound_star(self, path):
+        self.sound_star = pygame.mixer.Sound(os.path.join('data', 'sound', path))
+        self.sound_star.set_volume(0.054)
+
+    def set_sound_jump(self, path):
+        self.sound_jump = pygame.mixer.Sound(os.path.join('data', 'sound', path))
+        self.sound_jump.set_volume(0.1)
+
+    def set_sound_drop(self, path):
+        self.sound_drop = pygame.mixer.Sound(os.path.join('data', 'sound', path))
+        self.sound_drop.set_volume(0.045)
+
+    def set_sound_take_a_life(self, path):
+        self.sound_take_a_life = pygame.mixer.Sound(os.path.join('data', 'sound', path))
+
+        
 
 
 class Tile(pygame.sprite.Sprite):
@@ -74,6 +107,7 @@ class Player(pygame.sprite.Sprite):
         self.frame_index = 0
         self.current_x = 0
         self.animation_speed = 0.1
+        self.end = False
 
         self.status = 'idle'
         self.facing_right = True
@@ -89,11 +123,11 @@ class Player(pygame.sprite.Sprite):
         self.on_ground = False
 
     def import_character_assets(self):
-        character_path = 'data/character/'
+        character_path = os.path.join('data', 'character')
         self.animations = {'idle': [], 'run': [], 'jump': [], 'fall': []}
 
         for animation in self.animations.keys():
-            full_path = character_path + animation
+            full_path = os.path.join(character_path, animation)
             self.animations[animation] = import_folder(full_path)
 
     def animate(self):
@@ -117,8 +151,16 @@ class Player(pygame.sprite.Sprite):
             self.status = 'fall'
         else:
             if self.direction.x != 0:
+                # sound
+                if self.status == 'fall':
+                    sounds.sound_drop.play()
+                # sound
                 self.status = 'run'
             else:
+                # sound
+                if self.status == 'fall':
+                    sounds.sound_drop.play()
+                # sound
                 self.status = 'idle'
                 self.animation_speed = 0.1
 
@@ -128,14 +170,24 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
             self.facing_right = True
+            # sound
+            if not sounds.music_running.get_busy() and self.on_ground:
+                sounds.music_running.play()
+
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
             self.facing_right = False
+            # sound
+            if not sounds.music_running.get_busy() and self.on_ground:
+                sounds.music_running.play()
         else:
             self.direction.x = 0
 
         if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
+            # sound
+            sounds.sound_jump.play()
+            
 
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -143,11 +195,30 @@ class Player(pygame.sprite.Sprite):
 
     def jump(self):
         self.direction.y = self.jump_speed
-
+        
     def update(self):
         self.get_input()
         self.get_status()
         self.animate()
+        if self.rect.right + level.world_shift_sum + tile_size * 2 >= (len_level_x - 1) * tile_size:
+            self.end = True
+
+
+class BackGround:
+    def __init__(self, path):
+        self.path = path
+        self.x_left = level.player.sprite.rect.x
+        self.x_back_ground = 0
+        self.x = 0
+        self.word_shift = 0
+
+    def draw_and_update(self):
+        self.word_shift += level.world_shift
+        if level.world_shift:
+            self.x = 0 + self.word_shift
+            screen.blit(self.path, (self.x / 20, 0))
+        else:
+            screen.blit(self.path, (self.x / 20, 0))
 
 
 class Decor(pygame.sprite.Sprite):
@@ -166,6 +237,7 @@ class Level:
         self.setup_level(level_data)
         self.live_img = load_image('lives.png')
         self.lives = [(20 * (i + 1) + 5 * i, 20) for i in range(3)]
+        self.world_shift_sum = 0
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -174,7 +246,6 @@ class Level:
         self.player = pygame.sprite.GroupSingle()
         self.seaweeds = pygame.sprite.Group()
         self.world_shift = 0
-
 
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
@@ -186,7 +257,7 @@ class Level:
                     self.tiles.add(tile)
 
                 if cell == '|':
-                    seaweed = Decor((x, y), tile_size, f'seaweeds\seaweed{random.randint(1, 9)}.png')
+                    seaweed = Decor((x, y), tile_size, os.path.join('seaweeds', f'seaweed{random.randint(1, 9)}.png'))
                     self.seaweeds.add(seaweed)
 
                 if cell == '@':
@@ -207,9 +278,13 @@ class Level:
             self.world_shift = 4
             player.speed = 0
 
+            self.world_shift_sum -= 4
+
         elif player_x > width / 4 * 3 and direction > 0 and tiles[-1].rect.x - 4 >= 1280:
             self.world_shift = -4
             player.speed = 0
+
+            self.world_shift_sum += 4
         else:
             self.world_shift = 0
             player.speed = 4
@@ -217,6 +292,8 @@ class Level:
     def live(self):
         if self.lives:
             self.lives.pop(-1)
+            # sound
+            sounds.sound_take_a_life.play()
 
     def horizontal_movement_collision(self):
         player = self.player.sprite
@@ -249,9 +326,25 @@ class Level:
             if player.rect.colliderect(pygame.rect.Rect(sprite.rect.x + 10, sprite.rect.y + 10, 20, 20)):
                 self.player_stars += 1
                 self.stars.remove_internal(sprite)
+                # sound
+                sounds.sound_star.play()
 
         if player.on_ground and (player.direction.y < 0 or player.direction.y > 1):
             player.on_ground = False
+
+
+    def draw_end(self):
+        def text(list_text):
+            f1 = pygame.font.Font(None, 50)
+            for number, i in enumerate(list_text):
+                text1 = f1.render(i, True, 'black')
+                self.screen_end.blit(text1, ((list_x_y[number], 142)))
+        self.screen_end = load_image('ending.png')
+        list_x_y = ((self.screen_end.get_width() / 4.2), (self.screen_end.get_width() / 2), (self.screen_end.get_width() / 1.25))
+        text([str(self.player_stars), 'N', 'N'])
+
+        screen.blit(self.screen_end, (width / 2 - self.screen_end.get_width() / 2, height / 2 - self.screen_end.get_height() / 2))
+
 
     def draw(self):
         self.tiles.update(self.world_shift)
@@ -260,27 +353,41 @@ class Level:
         self.stars.draw(self.display_surface)
         self.scroll_x()
 
-        self.player.update()
-        self.horizontal_movement_collision()
-        self.vertical_movement_collision()
-        self.player.draw(self.display_surface)
-        for l in self.lives:
-            self.display_surface.blit(self.live_img, l)
-        self.seaweeds.update(self.world_shift)
-        self.seaweeds.draw(self.display_surface)
+        
+        if self.player.sprite.end:
+            self.draw_end()
+        else:
+            self.player.update()
+            self.horizontal_movement_collision()
+            self.vertical_movement_collision()
+            self.player.draw(self.display_surface)
+            for l in self.lives:
+                self.display_surface.blit(self.live_img, l)
+            self.seaweeds.update(self.world_shift)
+            self.seaweeds.draw(self.display_surface)
 
 
-tile_images = {x: load_image(f'tiles\\tile0{str(x).rjust(2, "0")}.png') for x in range(21)}
-bg = load_image('background.png')
+
+tile_images = {x: load_image(os.path.join('tiles', f'tile0{str(x).rjust(2, "0")}.png')) for x in range(21)}
 level = Level(level, screen)
+
+bg_photo = pygame.transform.scale(load_image('background.png'), (2000, 720))
+bg = BackGround(bg_photo)
+
+sounds = Sounds(path_jump='sound_jump.mp3', 
+                path_drop='sound_drop.mp3', 
+                path_running='sound_running.mp3', 
+                path_star='sound_star.mp3', 
+                path_take_a_life='sound_take_a_life.mp3')
+
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.blit(bg, (0, 0))
+    bg.draw_and_update()
     level.draw()
     pygame.display.flip()
     clock.tick(60)
